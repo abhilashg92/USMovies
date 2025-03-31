3d0d798c8ecce93b8e79ee660a31cc0a574c6895
abhilashg92 <ghogaleabhilash92@gmail.com>
2025-03-31 19:08:05 +0530

    Initial Commit

---
+++ USMovies/USMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  USMoviesApp.swift
+//  USMovies
+//
+//  Created by Abhilash Ghogale on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct USMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
